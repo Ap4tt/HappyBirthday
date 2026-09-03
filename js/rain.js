e74f6397b4213d24a223a900da42e70768433582
unknown <appaaatttt2@gmail.com>
2026-09-03 23:52:14 +0700

    mobile rain

--- js/rain.js
+++ js/rain.js
@@ -1,6 +1,6 @@
 (function () {
   const RAIN_SRCS = ['images/Flower1.png', 'images/Flower2.png', 'images/Heart1.png', 'images/Heart2.png'];
-  const RAIN_COUNT = window.innerWidth <= 640 ? 17 : 30;
+  const RAIN_COUNT = window.innerWidth <= 640 ? 22 : 30;
   const OPEN_DURATION = 420; // ms, harus match .wish-box.is-opening di CSS
   const CLOSE_DURATION = 320; // ms, harus match .wish-box.is-closing di CSS
 
@@ -198,7 +198,7 @@
     el.className = 'rain-item';
 
     const isMobile = window.innerWidth <= 640;
-    const size = isMobile ? (22 + Math.random() * 20) : (26 + Math.random() * 28);
+    const size = isMobile ? (22 + Math.random() * 22) : (26 + Math.random() * 28);
     const left = Math.random() * 100;
     const duration = isMobile ? (7 + Math.random() * 8) : (5 + Math.random() * 10);
     const delay = Math.random() * +duration; 
