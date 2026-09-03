a308e8672ff2b1a45b210881ba2126dd606c7d41
unknown <appaaatttt2@gmail.com>
2026-09-04 00:03:19 +0700

    bcontect font, rain images, burst ratio

--- js/rain.js
+++ js/rain.js
@@ -1,6 +1,6 @@
 (function () {
-  const RAIN_SRCS = ['images/Heart1.png', 'images/Heart2.png'];
-  const RAIN_COUNT = window.innerWidth <= 640 ? 22 : 30;
+  const RAIN_SRCS = ['images/Flower1.png', 'images/Flower2.png', 'images/Heart1.png', 'images/Heart2.png'];
+  const RAIN_COUNT = window.innerWidth <= 640 ? 20 : 30;
   const OPEN_DURATION = 420; // ms, harus match .wish-box.is-opening di CSS
   const CLOSE_DURATION = 320; // ms, harus match .wish-box.is-closing di CSS
 
